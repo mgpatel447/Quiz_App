36a65278d4d10d69dc9342317826565acffe1487
manjeet <mjpatel447@gmail.com>
2023-03-04 13:32:56 +0530

    Upload file for Quiz App

---
+++ app/src/androidTest/java/com/example/vikasojha/quizbee/ApplicationTestCase.java
@@ -0,0 +1,10 @@
+package com.example.vikasojha.quizbee;
+
+import android.app.Application;
+
+public
+class ApplicationTestCase<T> {
+    public
+    ApplicationTestCase(Class<Application> applicationClass) {
+    }
+}
